9ff0b76534c9ffbe640ec22bbe195a26d7341e3e
cristian <cpulido@unal.edu.co>
2019-06-20 16:34:05 -0500

    ACDVPR

--- estructurada.py
+++ estructurada.py
@@ -19,15 +19,38 @@ def limpiar(data,colum_to_hash,lista_eliminar):
         
     return data
 
+def limpiar_texto(data,colum_to_hash,lista_eliminar):
+    #print(list(data.columns))      
+    for i in lista_eliminar:     
+        data.drop(i,axis=1,inplace=True)
+
+    
+    for colum in colum_to_hash:
+        A=[]
+        for i in list(data[colum]):
+            try:
+                A.append(hashlib.md5(str(i).encode('utf-8')).hexdigest())
+            except:
+                A.append(i)
+        data.drop(colum,axis=1,inplace=True)
+                
+        data[colum]=A
+    
+    #data.drop(colum_to_hash,axis=1,inplace=True)
+        
+    return data    
+
 def limpiar_csv(path_in,path_out,colum_to_hash,lista_eliminar,delimiter="~"):
     #print(lista_eliminar)
     try:
         data=pd.read_csv(path_in,delimiter,encoding='utf-8')
     except:
         data=pd.read_excel(path_in,encoding='utf-8')
-    new_data=limpiar(data,colum_to_hash,lista_eliminar)
+    new_data=limpiar_texto(data,colum_to_hash,lista_eliminar)
     new_data.to_csv(path_out,sep=delimiter,index=False,encoding='utf-8')
     
     return 'completo'
 
-    
+
+lista=['PrimerNombre', 'SegundoNombre', 'PrimerApellido','SegundoApellido', 'Documento']
+limpiar_csv("/media/cpulido/Hitachi/universidad nacional/CONTRAREFERENCIA/IPES  Ubicacion contacto y acceso a oferta Victimas MY 16.xlsx","/home/cpulido/Escritorio/pregunta2/CONTRAREFERENCIA/IPES  Ubicacion contacto y acceso a oferta Victimas MY 16.csv",lista,[])
